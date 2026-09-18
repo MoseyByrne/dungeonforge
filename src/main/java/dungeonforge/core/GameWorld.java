64f69694867197a7fa8769cd3993f87914f9fef6
Mo Byrne <mosey.b.byrne@gmail.com>
2026-09-18 15:14:34 -0700

    Finish US-2.1

--- src/main/java/dungeonforge/core/GameWorld.java
+++ src/main/java/dungeonforge/core/GameWorld.java
@@ -2,6 +2,7 @@ package dungeonforge.core;
 
 import dungeonforge.config.GameConfig;
 import dungeonforge.config.RandomSource;
+import dungeonforge.factory.MonsterFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,9 +16,9 @@ import java.util.List;
  */
 public class GameWorld {
 
-
     private final Player player;
     private final List<DungeonLevel> levels = new ArrayList<>();
+    private MonsterFactory monsterFactory = new MonsterFactory();
 
     public GameWorld(Player player) {
         this.player = player;
@@ -36,18 +37,13 @@ public class GameWorld {
                 Room room = new Room("L" + d + "R" + r);
                 int count = RandomSource.getInstance().nextInt(maxMonstersPerRoom + 1);
                 for (int m = 0; m < count; m++) {
-                    room.addMonster(spawn(d));
+                    String id = RandomSource.getInstance().pick(monsterFactory.idsForTheme("crypt").toArray(new String[0]));
+                    room.addMonster(monsterFactory.create(id, d));
                 }
                 level.addRoom(room);
             }
             levels.add(level);
         }
-    }
-
-    private Monster spawn(int depth) {
-        String[] species = {"Skeleton", "Crypt Rat", "Wight", "Bone Priest"};
-        String pick = RandomSource.getInstance().pick(species);
-        return new Monster(pick, 12 + depth * 4, 4 + depth, 6 + depth * 3);
     }
 
     public Player getPlayer()             { return player; }
